1b52b3d04c2509f8a01a42cd604a8a16d97d927e
muhammad khan ahmad <rockzzz0092@gmail.com>
2024-11-25 14:39:48 +0400

    base

---
+++ app/src/main/java/com/example/edushieldpro/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.edushieldpro
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import com.example.edushieldpro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var currentSection = "login"
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onLoginClickListener()
+        onSignUpClickListener()
+
+    }
+
+    private fun onLoginClickListener() {
+        binding.btnLog.setOnClickListener {
+            if(currentSection!="login"){
+                it.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(this, R.color.black)
+                )
+                binding.btnSign.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(this, R.color.green)
+                )
+                currentSection = "login"
+                findNavController(R.id.fragmentContainerView).navigate(R.id.action_signUpFragment_to_logInFragment)
+            }
+        }
+    }
+
+    private fun onSignUpClickListener() {
+        binding.btnSign.setOnClickListener {
+            if(currentSection!="sign"){
+                currentSection = "sign"
+                it.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(this, R.color.black)
+                )
+                binding.btnLog.backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(this, R.color.green)
+                )
+                findNavController(R.id.fragmentContainerView).navigate(R.id.action_logInFragment_to_signUpFragment)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/edushieldpro/SignUpFragment.kt
@@ -0,0 +1,21 @@
+package com.example.edushieldpro
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.edushieldpro.databinding.FragmentSignUpBinding
+
+
+class SignUpFragment : Fragment(){
+    private lateinit var binding: FragmentSignUpBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
